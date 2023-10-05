1a88b0f50d4733e520e1c7dc2961db7eb3834400
Benjamin Sam <bsam079@uottawa.ca>
2023-10-05 03:18:49 -0400

    Adding main activity, adding basic login functionality on app start.

---
+++ app/src/main/java/com/example/cyclingmobileapp/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.cyclingmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginButtonClick(View view){
+        String username = ((EditText) findViewById(R.id.loginUsernameInput)).getText().toString();
+        String password = ((EditText) findViewById(R.id.loginPasswordInput)).getText().toString();
+
+        if (!validateLogin(username, password)){
+            Toast.makeText(this, "Invalid username or password! Try again.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+        mainActivityIntent.putExtra("username", username);
+        mainActivityIntent.putExtra("role", "admin");
+        startActivity(mainActivityIntent);
+        finish();
+    }
+
+    private boolean validateLogin(String username, String password){
+        if (username.equals("admin")){
+            return true;
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/example/cyclingmobileapp/MainActivity.java
+++ app/src/main/java/com/example/cyclingmobileapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cyclingmobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String username = getIntent().getExtras().getString("username");
+        String role = getIntent().getExtras().getString("role");
+
+        TextView greetingText = (TextView) findViewById(R.id.greetingText);
+        String greeting = "Hello " + username + "! You are logged in as: " + role + ".";
+        greetingText.setText(greeting);
     }
 }
