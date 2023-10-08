024c1ba72478aa5544e89f2fdd1eb59e9da76a8f
Benjamin Sam <bsam079@uottawa.ca>
2023-10-08 05:41:23 -0400

    Adding basic login using querying to the Firebase db

--- app/src/main/java/com/example/cyclingmobileapp/LoginActivity.java
+++ app/src/main/java/com/example/cyclingmobileapp/LoginActivity.java
@@ -6,33 +6,65 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.cyclingmobileapp.lib.user.Account;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.db = FirebaseFirestore.getInstance();
     }
 
     public void onLoginButtonClick(View view) {
         String username = ((EditText) findViewById(R.id.loginUsernameInput)).getText().toString();
         String password = ((EditText) findViewById(R.id.loginPasswordInput)).getText().toString();
 
-        if (!validateLogin(username, password)) {
-            Toast.makeText(this, "Invalid username or password! Try again.", Toast.LENGTH_SHORT).show();
-            return;
+        if (validateLogin(username, password)) {
+            db.collection("users").whereEqualTo("username", username).whereEqualTo("password", password).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot queryDocumentSnapshots = task.getResult();
+                        List<DocumentSnapshot> documentSnapshots = queryDocumentSnapshots.getDocuments();
+                        if (documentSnapshots.size() == 1) {
+                            Account account = documentSnapshots.get(0).toObject(Account.class);
+                            Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                            mainActivityIntent.putExtra("username", account.getUsername());
+                            mainActivityIntent.putExtra("role", account.getRole());
+                            startActivity(mainActivityIntent);
+                            finish();
+                            return;
+                        }
+                    }
+                    makeToast("Invalid username or password! Try again.");
+                }
+            });
+        } else {
+            makeToast("The username or password is invalid!");
         }
-
-        Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
-        mainActivityIntent.putExtra("username", username);
-        mainActivityIntent.putExtra("role", "admin");
-        startActivity(mainActivityIntent);
-        finish();
     }
 
     private boolean validateLogin(String username, String password) {
-        return username.equals("admin");
+        // Possibly use regex to validate the fields for login
+        // For now, just ensure that the username field doesn't have a space
+        return username.indexOf(" ") == -1;
+    }
+
+    private void makeToast(String toastText) {
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/cyclingmobileapp/lib/user/Account.java
@@ -0,0 +1,103 @@
+package com.example.cyclingmobileapp.lib.user;
+
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.32.1.6535.66c005ced modeling language!*/
+
+
+/**
+ * Consider proper access modifiers (ex: private, package, etc.) and getters/setters?
+ */
+// line 4 "model.ump"
+// line 62 "model.ump"
+public class Account {
+
+    //------------------------
+    // MEMBER VARIABLES
+    //------------------------
+
+    //Account Attributes
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+    //------------------------
+    // CONSTRUCTOR
+    //------------------------
+
+    public Account(){
+
+    }
+    public Account(String aUsername, String aEmail, String aPassword, String aRole) {
+        username = aUsername;
+        email = aEmail;
+        password = aPassword;
+        role = aRole;
+    }
+
+    //------------------------
+    // INTERFACE
+    //------------------------
+
+    public boolean setUsername(String aUsername) {
+        boolean wasSet = false;
+        username = aUsername;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setEmail(String aEmail) {
+        boolean wasSet = false;
+        email = aEmail;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setPassword(String aPassword) {
+        boolean wasSet = false;
+        password = aPassword;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setRole(String aRole) {
+        boolean wasSet = false;
+        role = aRole;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Email is required? Project doc mentions "you can???t enter an invalid email"
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void delete() {
+    }
+
+
+    public String toString() {
+        return super.toString() + "[" +
+                "username" + ":" + getUsername() + "," +
+                "email" + ":" + getEmail() + "," +
+                "password" + ":" + getPassword() + "," +
+                "role" + ":" + getRole() + "]";
+    }
+}
+
+
+
