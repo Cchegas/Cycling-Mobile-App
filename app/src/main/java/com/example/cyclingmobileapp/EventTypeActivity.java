4ee6c8e2722ddfe24aac49d82db67dd03350c7b2
Benjamin Sam <bsam079@uottawa.ca>
2023-11-08 18:58:15 -0500

    Updating strings/labels/titles on activities/fragments

--- app/src/main/java/com/example/cyclingmobileapp/EventTypeActivity.java
+++ app/src/main/java/com/example/cyclingmobileapp/EventTypeActivity.java
@@ -17,7 +17,7 @@ public class EventTypeActivity extends AppCompatActivity {
         String eventTypeLabel = getIntent().getExtras() != null ? getIntent().getExtras().getString("label") : null;
         TextView eventTypeHeader = (TextView) findViewById(R.id.eventTypeHeader);
         if (eventTypeLabel != null){
-            eventTypeHeader.setText(eventTypeLabel);
+            eventTypeHeader.setText("Modify event type");
         } else {
             eventTypeHeader.setText("Add an event type");
         }
