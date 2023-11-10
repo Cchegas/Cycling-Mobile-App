5912669f90486a4071e5bac94a069f129bd458b3
mksgandia <mgand060@uottawa.ca>
2023-11-10 09:52:35 -0500

    unused implementions

---
+++ app/src/main/java/com/example/cyclingmobileapp/CreateEventTypeActivity.java
@@ -0,0 +1,14 @@
+package com.example.cyclingmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateEventTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event_type);
+    }
+}
--- app/src/main/java/com/example/cyclingmobileapp/lib/user/AdminAccount.java
+++ app/src/main/java/com/example/cyclingmobileapp/lib/user/AdminAccount.java
@@ -1,5 +1,25 @@
 package com.example.cyclingmobileapp.lib.user;
 
+
+
+import android.widget.AdapterView ;
+import android.widget.Button;
+import android.widget.EditText ;
+import android.widget.ListView ;
+import android.widget.Spinner ;
+import android.widget.TextView ;
+import android.widget.Toast ;
+import android.content.Context ;
+
+import com.google.firebase.database.DataSnapshot ;
+import com.google.firebase.database.DatabaseError ;
+import com.google.firebase.database.DatabaseReference ;
+import com.google.firebase.database.FirebaseDatabase ;
+import com.google.firebase.database.ValueEventListener ;
+import java.util.ArrayList ;
+import java.util.List ;
+
+
 /*PLEASE DO NOT EDIT THIS CODE*/
 /*This code was generated using the UMPLE 1.32.1.6535.66c005ced modeling language!*/
 
@@ -38,6 +58,33 @@ public class AdminAccount extends Account {
         throw new UnsupportedOperationException();
     }
 
+
+    //ALL CODE BELOW WAS ADDED BY MARKUS
+    //---------------------------
+    // EVENT TYPE CREATION/DELETION
+    //---------------------------
+    public boolean createEventType() {
+        return false ;
+    }
+
+    public boolean deleteEventType() {
+        return false ;
+    }
+
+    //---------------------------
+    // ACCOUNT DELETION METHODS
+    //---------------------------
+    public boolean deleteClubAccount() {
+        return false ;
+    }
+
+    //Toast will appear on the CreateEventTypeActivity
+    public boolean deleteParticipantAccount(String username) {
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("users").child(username) ; //not sure what the reference is
+        dR.removeValue() ;
+        //Toast.makeText(getApplicationContext(),"Deleted participant account",Toast.LENGTH_LONG).show() ;
+        return true;
+    }
 }
   
   
