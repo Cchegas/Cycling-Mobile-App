e2a75df32c735df09c0ae9416eb5f188b3c9faa8
Benjamin Sam <bsam079@uottawa.ca>
2023-11-28 21:35:20 -0500

    Removing broken and unused imports

--- app/src/main/java/com/example/cyclingmobileapp/ProfileActivity.java
+++ app/src/main/java/com/example/cyclingmobileapp/ProfileActivity.java
@@ -1,8 +1,9 @@
 package com.example.cyclingmobileapp;
+
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,35 +11,32 @@ import com.example.cyclingmobileapp.lib.event.Event;
 import com.example.cyclingmobileapp.lib.event.EventType;
 import com.example.cyclingmobileapp.lib.user.ClubAccount;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProfileActivity extends AppCompatActivity{
-    ClubAccount account ;
-    EventTypeList offeredEventTypes ;
-    Event[] scheduledEvents ;
+public class ProfileActivity extends AppCompatActivity {
+    ClubAccount account;
+    EventTypeList offeredEventTypes;
+    Event[] scheduledEvents;
 
     private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile) ;
+        setContentView(R.layout.activity_profile);
 
-        db =  FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         List<EventType> eventTypeList = new ArrayList<>();
-        offeredEventTypes = new EventTypeList(this,eventTypeList) ;
+        offeredEventTypes = new EventTypeList(this, eventTypeList);
 
 
         //makes the list invisible unless the button is clicked
         Button button = findViewById(R.id.seeEventTypes);
         ListView listView = findViewById(R.id.offeredEventTypeListView);
-        listView.setVisibility(View.GONE) ;
+        listView.setVisibility(View.GONE);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
