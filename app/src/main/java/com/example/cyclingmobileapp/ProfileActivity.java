acf74cd64e50b9877c66d765aff76769ed9057ca
Benjamin Sam <bsam079@uottawa.ca>
2023-12-06 14:39:35 -0500

    Adding basic event display on profile activity

--- app/src/main/java/com/example/cyclingmobileapp/ProfileActivity.java
+++ app/src/main/java/com/example/cyclingmobileapp/ProfileActivity.java
@@ -1,14 +1,15 @@
 package com.example.cyclingmobileapp;
 
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import com.example.cyclingmobileapp.lib.event.Event;
 import com.example.cyclingmobileapp.lib.user.Account;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -16,12 +17,19 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private String username;
     private String clubUsername;
+    private ListView requiredFieldListView;
+    private List<Event> events;
+    private List<String> eventsDocumentIds;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,23 +41,30 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        events = new ArrayList<>();
+        eventsDocumentIds = new ArrayList<>();
+
+        requiredFieldListView = findViewById(R.id.profileEventsListView);
+
         if (getIntent().getExtras() != null) {
             username = getIntent().getExtras().getString("username");
             clubUsername = getIntent().getExtras().getString("clubUsername").toLowerCase();
         }
         displayAccountInfo();
+        updateEvents();
     }
 
-    private void displayAccountInfo(){
+    private void displayAccountInfo() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         TextView profileHeader = findViewById(R.id.profileHeader);
         TextView profileSocialMedia = findViewById(R.id.profileSocialMedia);
-        TextView  profileMainContact = findViewById(R.id.profileMainContact);
+        TextView profileMainContact = findViewById(R.id.profileMainContact);
         TextView profilePhoneNumber = findViewById(R.id.profilePhoneNumber);
         String defaultValue = "None";
         db.collection(Account.COLLECTION_NAME).whereEqualTo("username", clubUsername).get().addOnCompleteListener(task -> {
-            if (task.isSuccessful() && task.getResult().getDocuments().size() > 0){
+            if (task.isSuccessful() && task.getResult().getDocuments().size() > 0) {
                 DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+
                 String clubName = documentSnapshot.getString("name");
                 profileHeader.setText(clubName);
                 if (documentSnapshot.get("profileInfo") != null) {
@@ -62,9 +77,9 @@ public class ProfileActivity extends AppCompatActivity {
                     phoneNumber = phoneNumber == null ? defaultValue : phoneNumber;
                     socialMediaLink = socialMediaLink == null ? defaultValue : socialMediaLink;
 
-                    profileSocialMedia.setText(mainContact);
-                    profileMainContact.setText(phoneNumber);
-                    profilePhoneNumber.setText(socialMediaLink);
+                    profileSocialMedia.setText(socialMediaLink);
+                    profileMainContact.setText(mainContact);
+                    profilePhoneNumber.setText(phoneNumber);
                 } else {
                     profileSocialMedia.setText(defaultValue);
                     profileMainContact.setText(defaultValue);
@@ -74,6 +89,53 @@ public class ProfileActivity extends AppCompatActivity {
                 makeToast("Something went wrong! CLUB = " + clubUsername);
             }
         });
+    }
+
+    private void updateEvents(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection(Event.COLLECTION_NAME).whereEqualTo("organizer", clubUsername).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    List<DocumentSnapshot> documents = task.getResult().getDocuments();
+                    events.clear();
+                    eventsDocumentIds.clear();
+                    for (DocumentSnapshot document : documents) {
+                        String title = document.getString("title");
+
+                        Event event = new Event(title);
+                        events.add(event);
+                        eventsDocumentIds.add(document.getId());
+                    }
+                    updateEventListView();
+                } else {
+                    makeToast("Something went wrong when fetching events!");
+                    onSupportNavigateUp();
+                }
+            }
+        });
+    }
+
+    private ZonedDateTime parseDatetime(String datetime){
+        String datetimeWithoutTimezoneName = datetime.split("\\[")[0];
+        try {
+            return ZonedDateTime.parse(datetimeWithoutTimezoneName);
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    private boolean canRegisterForEvent(String datetime){
+        ZonedDateTime dateTime = parseDatetime(datetime);
+        if (dateTime == null){
+            return  false;
+        }
+        return !ZonedDateTime.now().isAfter(dateTime);
+    }
+
+    private void updateEventListView() {
+        EventList requiredFieldListAdapter = new EventList(this, events);
+        requiredFieldListView.setAdapter(requiredFieldListAdapter);
     }
 
     @Override
